7b0bf3edb8b66d274ff8a31d3ce8b2f22ba33624
unknown <javiermaker@gmail.com>
2018-09-20 11:42:13 +0200

    Primer commit

---
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrums',
+  templateUrl: './breadcrums.component.html',
+  styles: []
+})
+export class BreadcrumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
